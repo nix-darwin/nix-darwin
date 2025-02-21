dad152b6bea95ce823b7c79f1cb48e39c9752c82
angel <78835633+Iamanaws@users.noreply.github.com>
2025-02-21 12:44:13 -0800

    controlcenter: use string enums for consistency; add Spotlight module
    - Replace boolean options with descriptive string enums ("show"/"hide",
      "whenActive"/"always"/"hide", "both"/"menuBar"/"controlCenter"/"hide")
    - Add AccessibilityShortcuts, Battery, BatteryShowEnergyMode, Hearing,
      KeyboardBrightness, MusicRecognition, ScreenMirroring, StageManager,
      UserSwitcher, WiFi options
    - Deprecate boolean values with backward-compatible warnings
    - Add system.defaults.Spotlight.MenuItemHidden option

--- modules/system/defaults/controlcenter.nix
+++ modules/system/defaults/controlcenter.nix
@@ -1,100 +1,317 @@
 { config, lib, ... }:
+let
+  # Maps string values to macOS integers
+  mkApply =
+    mapping: default: v:
+    if v == null then null else mapping.${v} or default;
 
+  # Like mkApply but with deprecation warnings for boolean values
+  mkApplyWithBoolWarn =
+    mapping: boolMap: default: v:
+    if v == null then
+      null
+    else if v == true then
+      lib.warn boolMap.trueWarn (mapping.${boolMap.trueVal})
+    else if v == false then
+      lib.warn boolMap.falseWarn (mapping.${boolMap.falseVal})
+    else
+      mapping.${v} or default;
+
+  # "show" | "hide" (with deprecated bool)
+  visibilityType = lib.types.enum [
+    "show"
+    "hide"
+    true
+    false
+  ];
+  visibilityApply =
+    mkApplyWithBoolWarn
+      {
+        show = 18;
+        hide = 24;
+      }
+      {
+        trueWarn = "Boolean values are deprecated; use \"show\" instead";
+        trueVal = "show";
+        falseWarn = "Boolean values are deprecated; use \"hide\" instead";
+        falseVal = "hide";
+      }
+      18;
+
+  # "whenActive" | "always" | "hide" (with deprecated bool)
+  activeType = lib.types.enum [
+    "whenActive"
+    "always"
+    "hide"
+    true
+    false
+  ];
+  activeApply =
+    mkApplyWithBoolWarn
+      {
+        whenActive = 2;
+        always = 18;
+        hide = 8;
+      }
+      {
+        trueWarn = "Boolean values are deprecated; use \"always\" instead";
+        trueVal = "always";
+        falseWarn = "Boolean values are deprecated; use \"whenActive\" instead";
+        falseVal = "whenActive";
+      }
+      2;
+
+  # "both" | "menuBar" | "controlCenter" | "hide"
+  placementType = lib.types.enum [
+    "both"
+    "menuBar"
+    "controlCenter"
+    "hide"
+  ];
+  placementApply = mkApply {
+    both = 3;
+    menuBar = 6;
+    controlCenter = 9;
+    hide = 12;
+  } 12;
+
+  mkOpt =
+    {
+      type,
+      apply ? null,
+      description,
+    }:
+    lib.mkOption {
+      type = lib.types.nullOr type;
+      default = null;
+      inherit description;
+    }
+    // lib.optionalAttrs (apply != null) { inherit apply; };
+in
 {
-  options = {
+  options.system.defaults.controlcenter = {
 
-    system.defaults.controlcenter.BatteryShowPercentage = lib.mkOption {
-        type = lib.types.nullOr lib.types.bool;
-        default = null; 
-        description = ''
-            Apple menu > System Preferences > Control Center > Battery
+    AccessibilityShortcuts = mkOpt {
+      type = placementType;
+      apply = placementApply;
+      description = ''
+        Show Accessibility Shortcuts in Menu Bar and/or Control Center.
+        Options: "both", "menuBar", "controlCenter", "hide"
 
-            Show a battery percentage in menu bar. Default is null.
-        '';
+        Corresponds to: System Settings > Control Center > Accessibility Shortcuts
+      '';
     };
 
-    system.defaults.controlcenter.Sound = lib.mkOption {
-        type = lib.types.nullOr lib.types.bool;
-        apply = v: if v == null then null else if v == true then 18 else 24;
-        default = null; 
-        description = ''
-            Apple menu > System Preferences > Control Center > Sound
+    AirDrop = mkOpt {
+      type = visibilityType;
+      apply = visibilityApply;
+      description = ''
+        Show AirDrop in Menu Bar.
+        Options: "show", "hide"
 
-            Show a sound control in menu bar . Default is null.
-
-            18 = Display icon in menu bar
-            24 = Hide icon in menu bar
-        '';
+        Corresponds to: System Settings > Control Center > AirDrop
+      '';
     };
 
-    system.defaults.controlcenter.Bluetooth = lib.mkOption {
-        type = lib.types.nullOr lib.types.bool;
-        apply = v: if v == null then null else if v == true then 18 else 24;
-        default = null; 
-        description = ''
-            Apple menu > System Preferences > Control Center > Bluetooth
+    Battery = mkOpt {
+      type = placementType;
+      apply = mkApply {
+        both = 3;
+        menuBar = 4;
+        controlCenter = 9;
+        hide = 12;
+      } 4;
+      description = ''
+        Show Battery in Menu Bar and/or Control Center.
+        Options: "both", "menuBar", "controlCenter", "hide"
 
-            Show a bluetooth control in menu bar. Default is null.
-
-            18 = Display icon in menu bar
-            24 = Hide icon in menu bar
-        '';
+        Corresponds to: System Settings > Control Center > Battery
+      '';
     };
 
-    system.defaults.controlcenter.AirDrop = lib.mkOption {
-        type = lib.types.nullOr lib.types.bool;
-        apply = v: if v == null then null else if v == true then 18 else 24;
-        default = null; 
-        description = ''
-            Apple menu > System Preferences > Control Center > AirDrop
+    BatteryShowEnergyMode = mkOpt {
+      type = lib.types.enum [
+        "whenActive"
+        "always"
+      ];
+      apply = mkApply {
+        always = true;
+        whenActive = false;
+      } false;
+      description = ''
+        Show battery energy mode indicator.
+        Options: "whenActive", "always"
 
-            Show a AirDrop control in menu bar. Default is null.
-
-            18 = Display icon in menu bar
-            24 = Hide icon in menu bar
-        '';
+        Corresponds to: System Settings > Control Center > Battery > Show Energy Mode
+      '';
     };
 
-    system.defaults.controlcenter.Display = lib.mkOption {
-        type = lib.types.nullOr lib.types.bool;
-        apply = v: if v == null then null else if v == true then 18 else 24;
-        default = null; 
-        description = ''
-            Apple menu > System Preferences > Control Center > Display
+    BatteryShowPercentage = mkOpt {
+      type = lib.types.bool;
+      description = ''
+        Show battery percentage in Menu Bar.
 
-            Show a Screen Brightness control in menu bar. Default is null.
-
-            18 = Display icon in menu bar
-            24 = Hide icon in menu bar
-        '';
+        Corresponds to: System Settings > Control Center > Battery > Show Percentage
+      '';
     };
 
-    system.defaults.controlcenter.FocusModes = lib.mkOption {
-        type = lib.types.nullOr lib.types.bool;
-        apply = v: if v == null then null else if v == true then 18 else 24;
-        default = null; 
-        description = ''
-            Apple menu > System Preferences > Control Center > Focus
+    Bluetooth = mkOpt {
+      type = visibilityType;
+      apply = visibilityApply;
+      description = ''
+        Show Bluetooth in Menu Bar.
+        Options: "show", "hide"
 
-            Show a Focus control in menu bar. Default is null.
-
-            18 = Display icon in menu bar
-            24 = Hide icon in menu bar
-        '';
+        Corresponds to: System Settings > Control Center > Bluetooth
+      '';
     };
 
-    system.defaults.controlcenter.NowPlaying = lib.mkOption {
-        type = lib.types.nullOr lib.types.bool;
-        apply = v: if v == null then null else if v == true then 18 else 24;
-        default = null; 
-        description = ''
-            Apple menu > System Preferences > Control Center > Now Playing
+    Display = mkOpt {
+      type = activeType;
+      apply = activeApply;
+      description = ''
+        Show Display in Menu Bar.
+        Options: "whenActive", "always", "hide"
 
-            Show a Now Playing control in menu bar. Default is null.
-
-            18 = Display icon in menu bar
-            24 = Hide icon in menu bar
-        '';
+        Corresponds to: System Settings > Control Center > Display
+      '';
     };
+
+    FocusModes = mkOpt {
+      type = activeType;
+      apply = activeApply;
+      description = ''
+        Show Focus in Menu Bar.
+        Options: "whenActive", "always", "hide"
+
+        Corresponds to: System Settings > Control Center > Focus
+      '';
+    };
+
+    Hearing = mkOpt {
+      type = placementType;
+      apply = placementApply;
+      description = ''
+        Show Hearing in Menu Bar and/or Control Center.
+        Options: "both", "menuBar", "controlCenter", "hide"
+
+        Corresponds to: System Settings > Control Center > Hearing
+      '';
+    };
+
+    KeyboardBrightness = mkOpt {
+      type = placementType;
+      apply = placementApply;
+      description = ''
+        Show Keyboard Brightness in Menu Bar and/or Control Center.
+        Options: "both", "menuBar", "controlCenter", "hide"
+
+        Corresponds to: System Settings > Control Center > Keyboard Brightness
+      '';
+    };
+
+    MusicRecognition = mkOpt {
+      type = placementType;
+      apply = placementApply;
+      description = ''
+        Show Music Recognition in Menu Bar and/or Control Center.
+        Options: "both", "menuBar", "controlCenter", "hide"
+
+        Corresponds to: System Settings > Control Center > Music Recognition
+      '';
+    };
+
+    NowPlaying = mkOpt {
+      type = activeType;
+      apply = activeApply;
+      description = ''
+        Show Now Playing in Menu Bar.
+        Options: "whenActive", "always", "hide"
+
+        Corresponds to: System Settings > Control Center > Now Playing
+      '';
+    };
+
+    ScreenMirroring = mkOpt {
+      type = lib.types.enum [
+        "whenActive"
+        "always"
+        "hide"
+      ];
+      apply = activeApply;
+      description = ''
+        Show Screen Mirroring in Menu Bar.
+        Options: "whenActive", "always", "hide"
+
+        Corresponds to: System Settings > Control Center > Screen Mirroring
+      '';
+    };
+
+    Sound = mkOpt {
+      type = activeType;
+      apply = activeApply;
+      description = ''
+        Show Sound in Menu Bar.
+        Options: "whenActive", "always", "hide"
+
+        Corresponds to: System Settings > Control Center > Sound
+      '';
+    };
+
+    StageManager = mkOpt {
+      type = lib.types.enum [
+        "whenActive"
+        "hide"
+        true
+        false
+      ];
+      apply =
+        mkApplyWithBoolWarn
+          {
+            whenActive = 2;
+            hide = 8;
+          }
+          {
+            trueWarn = "Boolean values are deprecated; use \"whenActive\" instead";
+            trueVal = "whenActive";
+            falseWarn = "Boolean values are deprecated; use \"hide\" instead";
+            falseVal = "hide";
+          }
+          2;
+      description = ''
+        Show Stage Manager in Menu Bar.
+        Options: "whenActive", "hide"
+
+        Corresponds to: System Settings > Control Center > Stage Manager
+      '';
+    };
+
+    UserSwitcher = mkOpt {
+      type = placementType;
+      apply = mkApply {
+        both = 19;
+        menuBar = 22;
+        controlCenter = 25;
+        hide = 28;
+      } 28;
+      description = ''
+        Show Fast User Switching in Menu Bar and/or Control Center.
+        Options: "both", "menuBar", "controlCenter", "hide"
+
+        Corresponds to: System Settings > Control Center > Fast User Switching
+      '';
+    };
+
+    WiFi = mkOpt {
+      type = visibilityType;
+      apply = visibilityApply;
+      description = ''
+        Show Wi-Fi in Menu Bar.
+        Options: "show", "hide"
+
+        Corresponds to: System Settings > Control Center > Wi-Fi
+      '';
+    };
+
   };
 }
