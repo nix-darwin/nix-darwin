ac9b6015882e769a523d29d0f1ab74319547bce7
squat <lserven@gmail.com>
2025-05-23 20:02:08 +0200

    feat(services.openssh): add settings
    This commit introduces the `settings` option to configure OpenSSH.
    Most of the configuration options are lifted verbatim from
    https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/services/networking/ssh/sshd.nix
    with a few exceptions:
    1. `Macs` is renamed to `MACs` to comply with the upstream OpenSSH
       directive name;
    1. `PubkeyAuthentication` is introduced; this directive defaults to `no`
       on the OpenSSH version shipped with NixOS but defaults to `yes` on
       the OpenSSH version shipped with macOS;
    1. `Include` is introduced in order to mirror the default macOS
       configuration, which differs from the upstream OpenSSH default;
    1. `Subsystem` is introduced in order to mirror the default macOS
       configuration, which differs from the upstream OpenSSH default;
    1. `AuthorizedKeyFiles` is set to `[ ".ssh/authorized_keys" ]` to mirror
       the default macOS configuration, which differs from the upstream
       OpenSSH default.
    1. `UsePAM` defaults to `yes` on NixOS but defaults to `no`
       on the OpenSSH version shipped with NixOS but defaults to `yes` on
       the OpenSSH version shipped with macOS;
    
    Other notable settings:
    1. `KeyAlgorithms` uses the NixOS default rather than the default from
       the OpenSSH version that ships with macOS for better security.
    1. `MACs` uses the NixOS default rather than the default from
       the OpenSSH version that ships with macOS for better security.
    
    Signed-off-by: squat <lserven@gmail.com>

--- tests/services-openssh.nix
+++ tests/services-openssh.nix
@@ -1,12 +1,29 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 
 {
   services.openssh.extraConfig = ''
     StreamLocalBindUnlink yes
   '';
 
+  services.openssh.settings = {
+    MACs = [
+      "hmac-sha2-512-etm@openssh.com"
+      "hmac-sha2-256-etm@openssh.com"
+    ];
+    AllowGroups = [
+      "foo"
+      "bar"
+    ];
+  };
+
   test = ''
-    echo >&2 "checking for StreamLocalBindUnlink in /etc/ssh/ssh_known_hosts"
+    echo >&2 "checking for StreamLocalBindUnlink in /etc/ssh/sshd_config.d/100-nix-darwin.conf"
     grep 'StreamLocalBindUnlink yes' ${config.out}/etc/ssh/sshd_config.d/100-nix-darwin.conf
+
+    echo >&2 "checking for correct comma-separation in /etc/ssh/sshd_config.d/100-nix-darwin.conf"
+    grep --fixed-strings 'MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com' ${config.out}/etc/ssh/sshd_config.d/100-nix-darwin.conf
+
+    echo >&2 "checking for correct space-separation in /etc/ssh/sshd_config.d/100-nix-darwin.conf"
+    grep --fixed-strings 'AllowGroups foo bar' ${config.out}/etc/ssh/sshd_config.d/100-nix-darwin.conf
   '';
 }
