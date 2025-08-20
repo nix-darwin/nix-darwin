2fe9b5e660f5b5fd0c9e716305c806a6669c79de
imrunningaround <223467078+imrunningaround@users.noreply.github.com>
2025-08-20 11:28:15 -0400

    Update BatteryShowEnergyMode to handle false, and added tests.

--- modules/system/defaults/controlcenter.nix
+++ modules/system/defaults/controlcenter.nix
@@ -99,14 +99,15 @@
 
         system.defaults.controlcenter.BatteryShowEnergyMode = lib.mkOption {
         type = lib.types.nullOr lib.types.bool;
-        apply = v: if v == true then 1 else null;
+        apply = v: if v == null then null else if v then 1 else 0;
         default = null;
         description = ''
             Apple menu > System Settings > Control Center > Battery > Show Energy Mode
 
             Show the Energy Mode toggle in menu bar. Default is null.
 
-            1 = Display icon in the menu bar
+            true = Display icon in the menu bar
+            false = Show energy mode when active
         '';
     };
   };
