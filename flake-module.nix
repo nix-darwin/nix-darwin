a7ea1f727f880a86bcc13ba04e4c351716f12032
347Online | Katie Janzen <katiejanzen@347online.me>
2026-05-06 17:39:08 -0500

    flakeModules: add darwinModules option
    Mirrors structure of darwinConfigurations
    Description based on similar option for Home-Manager, see [<home-manager/flake-module.nix>](https://github.com/nix-community/home-manager/blob/00ed86e58bb6979a7921859fd1615d19382eac5c/flake-module.nix#L39-L41)

--- flake-module.nix
+++ flake-module.nix
@@ -3,9 +3,20 @@
   ...
 }:
 {
-  options.flake.darwinConfigurations = lib.mkOption {
-    type = lib.types.lazyAttrsOf lib.types.raw;
-    default = { };
-    description = "Darwin system configurations";
+  options.flake = {
+    darwinConfigurations = lib.mkOption {
+      type = lib.types.lazyAttrsOf lib.types.raw;
+      default = { };
+      description = "Darwin system configurations";
+    };
+    darwinModules = lib.mkOption {
+      type = lib.types.lazyAttrsOf lib.types.raw;
+      default = { };
+      description = ''
+        Darwin Modules
+
+        You may use this for reusable pieces of configuration, service modules, etc.
+      '';
+    };
   };
 }
