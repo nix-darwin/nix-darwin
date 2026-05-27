960d326cc230d1381a7bad1c045f80f5b5d1a37a
Proteus Qian <sudaku233@outlook.com>
2026-05-27 22:26:15 +0800

    modules/sing-box!: replace `configFile` with freeform `settings` and add secret support
    Replace the `configFile` path option with a structured `settings` option backed by `pkgs.formats.json`, enabling Nix-native sing-box configuration.
    
    Secret values can be set via `{ _secret = "/path/to/secret-file"; }` -- `utils.genJqSecretsReplacementSnippet` writes the final config to
    `/run/sing-box/config.json` at service startup via a `script` block, replacing the previous static `ProgramArguments` approach.
    
    BREAKING CHANGE: `services.sing-box.configFile` has been removed. Migrate to `services.sing-box.settings` with the sing-box configuration expressed
    as a Nix attribute set.

--- modules/services/sing-box.nix
+++ modules/services/sing-box.nix
@@ -2,19 +2,38 @@
   config,
   lib,
   pkgs,
+  utils,
   ...
 }:
 let
   cfg = config.services.sing-box;
+  settingsFormat = pkgs.formats.json { };
 in
 {
+  imports = [
+    (lib.mkRemovedOptionModule [ "services" "sing-box" "configFile" ] ''
+      The `configFile` option has been removed to support secret substitution.
+      Please migrate your configuration to `services.sing-box.settings`.
+    '')
+  ];
+
   meta.maintainers = [ lib.maintainers.proteus or "Proteus Qian" ];
   options.services.sing-box = {
     enable = lib.mkEnableOption "sing-box universal proxy platform";
     package = lib.mkPackageOption pkgs "sing-box" { };
-    configFile = lib.mkOption {
-      type = lib.types.path;
-      description = "Path to the sing-box config file";
+
+    settings = lib.mkOption {
+      type = lib.types.submodule {
+        freeformType = settingsFormat.type;
+      };
+      default = { };
+      description = ''
+        The sing-box configuration, see <https://sing-box.sagernet.org/configuration/> for documentation.
+
+        Options containing secret data should be set to an attribute set
+        containing the attribute `_secret` - a string pointing to a file
+        containing the value the option should be set to.
+      '';
     };
   };
   config = lib.mkIf cfg.enable {
@@ -27,21 +46,28 @@ in
         install -dm700 ${lib.escapeShellArg config.launchd.daemons.sing-box.serviceConfig.WorkingDirectory}
       fi
     '';
-    launchd.daemons.sing-box.serviceConfig = {
-      Label = "io.nekohasekai.sing-box";
-      RunAtLoad = true;
-      KeepAlive = {
-        Crashed = true;
-        SuccessfulExit = false;
+
+    launchd.daemons.sing-box = {
+      serviceConfig = {
+        Label = "io.nekohasekai.sing-box";
+        RunAtLoad = true;
+        KeepAlive = {
+          Crashed = true;
+          SuccessfulExit = false;
+        };
+        WorkingDirectory = "/Library/Application Support/sing-box";
+        StandardErrorPath = "/Library/Logs/io.nekohasekai.sing-box.stderr.log";
+        StandardOutPath = "/Library/Logs/io.nekohasekai.sing-box.stdout.log";
+        # ProgramArguments = [
+        #   "/bin/sh"
+        #   "-c"
+        #   ("/bin/wait4path /nix/store" + " && exec ${lib.getExe cfg.package} -c ${cfg.configFile} run")
+        # ];
       };
-      WorkingDirectory = "/Library/Application Support/sing-box";
-      StandardErrorPath = "/Library/Logs/io.nekohasekai.sing-box.stderr.log";
-      StandardOutPath = "/Library/Logs/io.nekohasekai.sing-box.stdout.log";
-      ProgramArguments = [
-        "/bin/sh"
-        "-c"
-        ("/bin/wait4path /nix/store" + " && exec ${lib.getExe cfg.package} -c ${cfg.configFile} run")
-      ];
+      script = ''
+        ${utils.genJqSecretsReplacementSnippet cfg.settings "/run/sing-box/config.json"}
+        "${lib.getExe cfg.package} -D ${config.launchd.daemons.sing-box.serviceConfig.WorkingDirectory} -C /run/sing-box run"
+      '';
     };
   };
 }
