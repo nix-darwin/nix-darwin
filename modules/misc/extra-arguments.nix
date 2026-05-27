604df6bf9663d72ac7a0da52960f223646fa7fda
Proteus Qian <sudaku233@outlook.com>
2026-05-27 23:19:58 +0800

    modules/extra-arguments: fix: use `pkgs.path` instead of `nixpkgs` input to load utils
    `nixpkgs` is not available as a top-level argument in tests, causing evaluation failures. Switch to `pkgs.path` which is always present when `pkgs` is in scope.

--- modules/misc/extra-arguments.nix
+++ modules/misc/extra-arguments.nix
@@ -2,11 +2,10 @@
   lib,
   config,
   pkgs,
-  nixpkgs,
   ...
 }:
 {
   _module.args = {
-    utils = import "${nixpkgs}/nixos/lib/utils.nix" { inherit pkgs lib config; };
+    utils = import "${pkgs.path}/nixos/lib/utils.nix" { inherit pkgs lib config; };
   };
 }
